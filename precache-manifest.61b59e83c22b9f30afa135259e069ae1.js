e0bce18cfb4d5ac9c62c7b1c0f569eee5b4fc253
Yael Tuvia <yael159753@gmail.com>
2020-04-12 13:13:47 +0300

    Updates

---
+++ precache-manifest.61b59e83c22b9f30afa135259e069ae1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "761f6202e971f02a9e3bb0081d653beb",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d1191bf25009cd1791e8",
+    "url": "/monsters-rolodex/static/css/main.a048fcfe.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1191bf25009cd1791e8",
+    "url": "/monsters-rolodex/static/js/main.40cd4511.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
